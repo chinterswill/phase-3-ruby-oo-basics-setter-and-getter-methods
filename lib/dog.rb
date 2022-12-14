1bfd1c73ed59f0a1f6dee590dd58ec274f8fb3d5
Kamran Hussein <chinterswill@gmail.com>
2022-12-14 13:10:11 -0500

    commit tree aab8d8eaba682929fa726126e3184a7be6cbdcb5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
-class Dog
+# class Dog
+#     attr_reader :name
+#     attr_accessor :breed
+#     def initialize(name, breed)
+#         @name = name
+#         @breed = breed
+#     end
+# end
 
+
+
+class Dog
+    def name=(name, breed)
+        @name = name
+        @breed = breed
+    end
+
+    def name
+        @name
+        @breed
+    end
 end
+
+fido = Dog.new
+fido.name = 'Fido'
+fido.breed = 'German Shepherd'
